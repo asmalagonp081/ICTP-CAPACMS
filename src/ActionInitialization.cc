0c7adca6cf6336a9fd0e55d2b379131440054e1a
asmalagonp081 <asmalagonp@gmail.com>
2026-02-23 11:20:07 -0500

    Creo la clase build

---
+++ src/ActionInitialization.cc
@@ -0,0 +1,4 @@
+// ---------- Comparative Analysis of Photon Attenuation Coefficients in Multilayer Shielding using Geant4 and MULASSIS -----
+// @author: Angela Sofia Malagon Palomino 
+// --------------------------------------------------------------------------------------------------------------------------
+// -- Build --
