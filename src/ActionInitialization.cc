6ac646117d5c0405ca61cb3c649ef9a7e8e6b700
asmalagonp081 <asmalagonp@gmail.com>
2026-03-03 20:45:53 -0500

    Modifico cositas :>

--- src/ActionInitialization.cc
+++ src/ActionInitialization.cc
@@ -2,3 +2,33 @@
 // @author: Angela Sofia Malagon Palomino 
 // --------------------------------------------------------------------------------------------------------------------------
 // -- Build --
+#include "ActionInitialization.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "SteppingAction.hh"
+
+ActionInitialization::ActionInitialization()
+ : G4VUserActionInitialization()
+{}
+
+ActionInitialization::~ActionInitialization()
+{}
+
+// Este método es para simulaciones que usan muchos núcleos del procesador (Multi-threading)
+void ActionInitialization::BuildForMaster() const
+{
+  SetUserAction(new RunAction);
+}
+
+// Este método es donde definimos qué hace cada "hilo" de trabajo
+void ActionInitialization::Build() const
+{
+  // 1. El que dispara: PrimaryGeneratorAction
+  SetUserAction(new PrimaryGeneratorAction);
+
+  // 2. El que anota el resumen: RunAction
+  SetUserAction(new RunAction);
+  
+  // 3. El que vigila cada paso: SteppingAction (lo crearemos después)
+  SetUserAction(new SteppingAction); 
+}
